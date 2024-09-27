f4d531dde0766c1f11dce4b60918da6774fd7e0f
williamrd360p <willian.silva579@etec.sp.gov.br>
2024-09-27 20:07:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿const double ReceitaFarinha = 1;
+const double ReceitaAgua = 0.7;
+const double ReceitaFermento = 0.4;
+const double ReceitaSal = 0.02;
+double percentualPaoRelativoFarinha = ReceitaFarinha + ReceitaAgua + ReceitaFermento + ReceitaSal;
+double pesoPao, farinha, agua, fermento, sal;
+Console.WriteLine("---Padaria---");
+Console.Write("Peso desejado de pão italiano (em gramas)");
+pesoPao = Convert.ToDouble(Console.ReadLine());
+farinha = pesoPao / percentualPaoRelativoFarinha;
+agua = farinha * ReceitaAgua;
+fermento = farinha * ReceitaFermento;
+sal = farinha * ReceitaSal;
+Console.WriteLine("Ingredientes:");
+Console.WriteLine($"Farinha: {farinha:N0} g");
+Console.WriteLine($"Água: {agua:N0} g");
+Console.WriteLine($"Fermento: {fermento:N0} g");
+Console.WriteLine($"Sal: {sal:N0} g");
